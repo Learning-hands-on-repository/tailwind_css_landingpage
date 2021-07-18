c3cb8ab7403198b560228ca8a0d58c390204389b
Nara <narawit.tub@outlook.co.th>
2021-07-18 19:51:36 +0700

    add font

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screen: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
