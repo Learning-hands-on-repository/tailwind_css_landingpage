e21b264c9161c1faf4883df670c1a30e66fa6201
Nara <narawit.tub@outlook.co.th>
2021-07-18 19:57:20 +0700

    extend color pattelte

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      },
+    },
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
     },
